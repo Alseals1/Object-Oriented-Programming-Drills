09c524f428c1db680637289b7d07852fbb6c900a
Alandis Seals <alandis.seals@yahoo.com>
2020-06-18 20:45:20 -0500

    Using Classes Inheritance

--- script.js
+++ script.js
@@ -2,31 +2,31 @@
 // Before ES6
 let person1 = {
     name: "Alandis Seals",
-    sayHello:function() {
+    sayHello: function () {
         console.log(`Hello! My name is ${this.name}`)
     }
 }
 let person2 = {
     name: "Aria",
-    sayHello:function() {
+    sayHello: function () {
         console.log(`Hello! My name is ${this.name}`)
     }
 }
 let person3 = {
     name: "Romel",
-    sayHello:function() {
+    sayHello: function () {
         console.log(`Hello! My name is ${this.name}`)
     }
 }
 let person4 = {
     name: "Fred",
-    sayHello:function() {
+    sayHello: function () {
         console.log(`Hello! My name is ${this.name}`)
     }
 }
 let person5 = {
     name: "Keke",
-    sayHello:function() {
+    sayHello: function () {
         console.log(`Hello! My name is ${this.name}`)
     }
 }
@@ -38,7 +38,7 @@ person4.sayHello()
 person5.sayHello()
 
 // Pseudo Classes and Methods
-function Person(name, city, age){
+function Person(name, city, age) {
     this.name = name
     this.city = city
     this.age = age
@@ -50,4 +50,44 @@ Person.prototype.sayHello = function () {
 }
 let p1 = new Person("Alandis", "Birminham", 25)
 p1.sayHello()
-    
+
+
+// Using Classes Inheritance
+class Vehicle {
+    constructor(wheel, manuf) {
+        this.wheel = wheel
+        this.manuf = manuf
+
+
+    }
+    aboutVehicle() {
+        console.log(`My vehicle is a ${this.manuf}and had ${this.wheel}`)
+    }
+}
+
+class Truck extends Vehicle {
+    constructor(wheel, manuf, numberOfDoors, hasBed) {
+        super(wheel, manuf);
+
+        this.numberOfDoors = numberOfDoors;
+        this.hasBed = hasBed;
+
+    }
+}
+
+class Sudan extends Vehicle {
+    constructor(wheel, manuf, size, GasMile) {
+        super(wheel, manuf)
+        this.GasMile = GasMile
+        this.size = size
+    }
+}
+
+class Motorcycle extends Vehicle{
+    constructor(wheel, manuf, hasHandleBars, hasDoor){
+        super(wheel, manuf)
+        this.hasHandleBars = hasHandleBars
+        this.hasDoor = hasDoor
+    }
+}
+
