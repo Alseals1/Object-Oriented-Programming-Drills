54cc1099e6a303cf64c2f28536a03b575ae6f045
Alandis Seals <alandis.seals@yahoo.com>
2020-06-18 19:34:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let person1 = {
+    name: "Alandis Seals",
+    sayHello:function() {
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+let person2 = {
+    name: "Aria",
+    sayHello:function() {
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+let person3 = {
+    name: "Romel",
+    sayHello:function() {
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+let person4 = {
+    name: "Fred",
+    sayHello:function() {
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+let person5 = {
+    name: "Keke",
+    sayHello:function() {
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+
+person1.sayHello()
+person2.sayHello()
+person3.sayHello()
+person4.sayHello()
+person5.sayHello()
